bd9e8fc7f0eb5bbb017468291b9e5623a1c4cc1c
TDragon <t.duong800@gmail.com>
2019-11-21 16:11:31 -0800

    initial commit

---
+++ js/script.js
@@ -0,0 +1,42 @@
+$(document).ready(function() {
+var magic8Ball = {};
+magic8Ball.listOfAnswers = ["yes", "no", "maybe", "try again", "it could happen", "you never know!"];
+
+  $("#answer").hide ();
+
+magic8Ball.questionMagic8Ball = function(question)
+{
+var randomNumber = Math.random();
+var randomNumberArray = randomNumber * this.listOfAnswers.length;
+var randomIndex = Math.floor(randomNumberArray);
+var answer = this.listOfAnswers [randomIndex];
+
+  $("#answer").text(answer);
+
+
+console.log(question);
+console.log(answer);
+};
+
+var onClick = function() {
+
+  $("#answer").hide ();
+
+  $("#answer").fadeIn(4000);
+
+  $("#8ball").attr ("src","https://s3.amazonaws.com/media.skillcrush.com/skillcrush/wp-content/uploads/2016/09/answerside.png");
+
+  $("#8ball").effect("shake");
+
+ setTimeout( function ()
+   {
+   var askQuestion = prompt("ASK ME ANYTHING!" , "Will I ever learn Javascript?");
+    magic8Ball.questionMagic8Ball("question");
+  }, 500);
+
+
+}; 
+
+$("#questionButton").click(onClick);
+
+});
